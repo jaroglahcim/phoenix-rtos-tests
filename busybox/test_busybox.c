cdff77be0cc8b3fb5edf89141d7bb12321960f13
Damian <damian.loewnau@gmail.com>
2021-08-13 10:52:38 +0200

    busybox: add strerror and _exit from child process when failed
    JIRA: RTOS-59

--- busybox/test_busybox.c
+++ busybox/test_busybox.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <dirent.h>
 #include <errno.h>
@@ -7,35 +8,37 @@
 int main(int argc, char **argv)
 {
 	int ret;
-	DIR* dir = opendir("/bin/testsuite");
+	DIR *dir = opendir("/bin/testsuite");
 
 	if (dir) {
 		closedir(dir);
-	} else if (ENOENT == errno) {
+	}
+	else if (ENOENT == errno) {
 		fprintf(stderr, "There is no busybox test suite to run, build project with \"long_test\"\n");
-		return 1;
-	} else {
-		fprintf(stderr, "There is problem with opening existing /bin/testsuite directory\n");
-		return 1;
+		return(1);
+	}
+	else {
+		fprintf(stderr, "There is problem with opening existing /bin/testsuite directory: %s\n", strerror(errno));
+		return(1);
 	}
 
 	if ((ret = vfork()) < 0) {
-		fprintf(stderr, "vfork function failed \n");
-		return 1;
+		fprintf(stderr, "vfork function failed: %s\n", strerror(errno));
+		return(1);
 	}
 	else if (!ret) {
 		execv("/bin/posixsrv", argv);
-		fprintf(stderr, "exec function failed \n");
-		return 1;
+		fprintf(stderr, "exec function failed: %s\n", strerror(errno));
+		_exit(EXIT_FAILURE);
 	}
 	printf("Posixsrv ran in background\n");
 
 	/* without this delay sometimes posixsrv is not yet running before the next function call */
-	usleep(500000);
+	usleep(100000);
 
 	if ((ret = system("cd /bin/testsuite/ && export PATH=/bin:/sbin:/usr/bin:/usr/sbin && ./runtest")) < 0) {
-		fprintf(stderr, "system function failed \n");
-		return 1;
+		fprintf(stderr, "system function failed: %s\n", strerror(errno));
+		return(1);
 	}
 
 	printf("\nRuntest done\n\n");
