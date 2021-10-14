2b3d9de09984e4df185b95f3f8db9da978d143cb
s171937 <gorajmichal@outlook.com>
2021-10-14 19:05:34 +0200

    cos(0) att 1

--- cos/cos.c
+++ cos/cos.c
@@ -10,12 +10,14 @@ TEST_TEAR_DOWN(test_cos)
 {
 }
 
-TEST(test_cos, test1)
+TEST(test_cos, cos0)
 {
-    TEST_FAIL_MESSAGE("Not implemented yet");
+    double res = cos(0);
+    TEST_ASSERT_EQUAL_INT(1, res);
+    //TEST_FAIL_MESSAGE("Not implemented yet");
 }
 
-TEST_GROUP_RUNNER(test_cos)
+TEST_GROUP_RUNNER(test_cos) 
 {
     RUN_TEST_CASE(test_cos, test1);
 }
