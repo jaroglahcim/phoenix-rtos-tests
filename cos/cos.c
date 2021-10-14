cd5229072ab29b85c6e693e4b2771e5db00dcd10
s171937 <gorajmichal@outlook.com>
2021-10-14 19:31:10 +0200

    cos 0 30 45 60 90

--- cos/cos.c
+++ cos/cos.c
@@ -1,25 +1,67 @@
+#include <math.h>
+#define UNITY_INCLUDE_DOUBLE
+
 #include "unity_fixture.h"
 
 TEST_GROUP(test_cos);
 
 TEST_SETUP(test_cos)
 {
+    double res, x, y;
 }
 
 TEST_TEAR_DOWN(test_cos)
 {
 }
 
-TEST(test_cos, cos0)
+TEST(test_cos, cos_0)
 {
-    double res = cos(0);
-    TEST_ASSERT_EQUAL_DOUBLE(1.0, res);
-    //TEST_FAIL_MESSAGE("Not implemented yet");
+    x = 0.0;
+    y = 1.0;
+    res = cos(x);
+    TEST_ASSERT_EQUAL_DOUBLE(y, res);
 }
+
+TEST(test_cos, cos_sixth_of_pi)
+{
+    x = M_PI / 6.0;
+    y = sqrt(3) / 2.0;
+    res = cos(x);
+    TEST_ASSERT_EQUAL_DOUBLE(y, res);
+}
+
+TEST(test_cos, cos_fourth_of_pi)
+{
+    x = M_PI / 4.0;
+    y = sqrt(2) / 2.0;
+    res = cos(0);
+    TEST_ASSERT_EQUAL_DOUBLE(y, res);
+}
+
+TEST(test_cos, cos_third_of_pi)
+{
+    x = M_PI / 3.0;
+    y = 0.5;
+    res = cos(0);
+    TEST_ASSERT_EQUAL_DOUBLE(y, res);
+}
+
+TEST(test_cos, cos_half_of_pi)
+{
+    x = M_PI / 2.0;
+    y = 0.0;
+    res = cos(0);
+    TEST_ASSERT_EQUAL_DOUBLE(y, res);
+}
+
 
 TEST_GROUP_RUNNER(test_cos) 
 {
-    RUN_TEST_CASE(test_cos, cos0);
+    RUN_TEST_CASE(test_cos, cos_0);
+    RUN_TEST_CASE(test_cos, cos_sixth_of_pi);
+    RUN_TEST_CASE(test_cos, cos_fourth_of_pi);
+    RUN_TEST_CASE(test_cos, cos_third_of_pi);
+    RUN_TEST_CASE(test_cos, cos_half_of_pi);
 }
 
 void runner(void)
