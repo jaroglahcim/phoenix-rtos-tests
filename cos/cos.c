dd9fa3c4bcc16133a83ba3109f0d54d4dacc57b8
s171937 <gorajmichal@outlook.com>
2021-10-19 22:23:44 +0200

    fixing copying errors

--- cos/cos.c
+++ cos/cos.c
@@ -42,7 +42,7 @@ TEST(test_cos, cos_pi_3)
 
 TEST(test_cos, cos_pi_2)
 {
-    TEST_ASSERT_DOUBLE_WITHIN(1e-12, 0.0, M_PI / 2.0);
+    TEST_ASSERT_DOUBLE_WITHIN(1e-15, 0.0, cos(M_PI / 2.0));
 }
 
 TEST(test_cos, cos_2_pi_3)
@@ -52,7 +52,7 @@ TEST(test_cos, cos_2_pi_3)
 
 TEST(test_cos, cos_3_pi_4)
 {
-    TEST_ASSERT_EQUAL_DOUBLE(-sqrt(2) / 2.0, cos(0.75* M_PI));
+    TEST_ASSERT_EQUAL_DOUBLE(-sqrt(2.0) / 2.0, cos(0.75* M_PI));
 }
 
 TEST(test_cos, cos_5_pi_6)
@@ -82,29 +82,28 @@ TEST(test_cos, cos_4_pi_3)
 
 TEST(test_cos, cos_3_pi_2)
 {
-    TEST_ASSERT_DOUBLE_WITHIN(1e-12, 0.0, cos(1.5 * M_PI));
+    TEST_ASSERT_DOUBLE_WITHIN(1e-15, 0.0, cos(1.5 * M_PI));
 }
 
 TEST(test_cos, cos_5_pi_3)
 {
-    TEST_ASSERT_EQUAL_DOUBLE(-sqrt(3) / 2.0, cos((7.0 / 6.0) * M_PI));
+    TEST_ASSERT_EQUAL_DOUBLE(0.5, cos((5.0 / 3.0) * M_PI));
 }
 
 TEST(test_cos, cos_7_pi_4)
 {
-    TEST_ASSERT_EQUAL_DOUBLE(-sqrt(2) / 2.0, cos(1.25 * M_PI));
+    TEST_ASSERT_EQUAL_DOUBLE(sqrt(2.0) / 2.0, cos(1.75 * M_PI));
 }
 
 TEST(test_cos, cos_11_pi_6)
 {
-    TEST_ASSERT_EQUAL_DOUBLE(-0.5, cos((4.0 / 3.0) * M_PI));
+    TEST_ASSERT_EQUAL_DOUBLE(sqrt(3.0) / 2.0, cos((11.0 / 6.0) * M_PI));
 }
 
 TEST(test_cos, cos_2_pi)
 {
-    TEST_ASSERT_DOUBLE_WITHIN(1e-12, 0.0, cos(1.5 * M_PI));
+    TEST_ASSERT_EQUAL_DOUBLE(1.0, cos(2.0 * M_PI));
 }
-
 
 
 TEST(test_cos, cos_NaN)
@@ -147,43 +146,7 @@ TEST_GROUP_RUNNER(test_cos_special_cases)
     RUN_TEST_CASE(test_cos, cos_minus_pi);
     RUN_TEST_CASE(test_cos, cos_100_pi);
     RUN_TEST_CASE(test_cos, cos_NaN);
-    RUN_TEST_CASE(test_cos, cos_minus_pi);
 }
-// TEST(test_cos, cos_second_quarter)
-// {
-//     TEST_ASSERT_EQUAL_DOUBLE(-0.5, cos((2.0 / 3.0) * M_PI));
-//     TEST_ASSERT_EQUAL_DOUBLE(-sqrt(2) / 2.0, cos(0.75* M_PI));
-//     TEST_ASSERT_EQUAL_DOUBLE(-sqrt(3) / 2.0, cos((5.0 / 6.0) * M_PI));
-//     TEST_ASSERT_EQUAL_DOUBLE(-1.0, cos(M_PI));
-// }
-
-// TEST(test_cos, cos_third_quarter)
-// {
-//     TEST_ASSERT_EQUAL_DOUBLE(-sqrt(3) / 2.0, cos((7.0 / 6.0) * M_PI));
-//     TEST_ASSERT_EQUAL_DOUBLE(-sqrt(2) / 2.0, cos(1.25 * M_PI));
-//     TEST_ASSERT_EQUAL_DOUBLE(-0.5, cos((4.0 / 3.0) * M_PI));
-//     //TEST_ASSERT_EQUAL_DOUBLE(0.0, cos(1.5 * M_PI));
-//     TEST_ASSERT_DOUBLE_WITHIN(1e-12, 0.0, cos(1.5 * M_PI));
-// }
-
-// TEST(test_cos, cos_fourth_quarter)
-// {
-//     TEST_ASSERT_EQUAL_DOUBLE(0.5, cos((5.0 / 3.0) * M_PI));
-//     TEST_ASSERT_EQUAL_DOUBLE(sqrt(2) / 2.0, cos(1.75 * M_PI));
-//     TEST_ASSERT_EQUAL_DOUBLE(sqrt(3) / 2.0, cos((11.0 / 6.0) * M_PI));
-//     TEST_ASSERT_EQUAL_DOUBLE(1.0, cos(2.0 * M_PI));
-// }
-
-
-// TEST_GROUP_RUNNER(test_cos_known_values_quarters)
-// {
-//     RUN_TEST_CASE(test_cos, cos_second_quarter);
-//     RUN_TEST_CASE(test_cos, cos_third_quarter);
-//     RUN_TEST_CASE(test_cos, cos_fourth_quarter);
-//     RUN_TEST_CASE(test_cos, NaN);
-//     RUN_TEST_CASE(test_cos, cos_-pi);
-//     RUN_TEST_CASE(test_cos, cos_100pi);
-// }
 
 void runner(void)
 {
